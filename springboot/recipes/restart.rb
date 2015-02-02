33a44123e31394b02010f96a1fa9a82d8a3f77b2
vdevil <vincent.devillers@catboatmedia.fr>
2015-02-02 17:34:44 +0100

    Update Chef configuration

--- springboot/recipes/restart.rb
+++ springboot/recipes/restart.rb
@@ -11,27 +11,37 @@ node[:deploy].each do |application, deploy|
     app application
   end
   
+  bash 'SpringBoot: Force java 1.8.0' do
+    user 'root'
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+        update-alternatives --set $(update-alternatives --display java | grep -v esclave | grep 1.8.0 | awk '{print $1}')
+    EOH
+  end
+
   bash 'SpringBoot: Shutting down the current running' do
     user 'root'
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
         curl -XPOST "http://localhost:8080/admin/shutdown" > /var/log/app.log 2>&1 || : 
-		RETVAL=$?
-		if [ $RETVAL -eq 0 ]
-		then
-			echo "Service stopped"
-		else
-			echo "Can not stop the service"
-		fi
-		exit 0
+	RETVAL=$?
+	if [ $RETVAL -eq 0 ]
+	then
+		echo "Service stopped"
+	else
+		echo "Can not stop the service"
+	fi
+	exit 0
     EOH
   end
   
+  java_opts = node['springboot']['java_opts']
+  log_file = node['springboot']['log_file']
   bash 'SpringBoot: Running the deployed' do
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-        java -jar *.jar > /var/log/app.log 2>&1 &
+        java #{java_opts} -Dlogging.file="#{log_file}" -jar *.jar > /dev/null 2>&1 &
     EOH
   end
   
